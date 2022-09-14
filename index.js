55f758779fd33eb738cb291f776e650054939ff4
Daniel Padrino <danswarrior1@gmail.com>
2022-09-14 11:56:11 -0300

    install client dependencie

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-exports.handler = async (event) => {
-    const response = "hello";
-    return response;
+// const { Spot } = require("@danswar/binance-connector-node");
+
+exports.handler = async () => {
+  // const client = new Spot(process.env.APP_KEY, process.env.APP_SECRET);
+  const response = "hello";
+  return response;
 };
