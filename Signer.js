3f6dc50cc846383a6f72fd0f58cf725d23b99f28
Daniel Padrino <danswarrior1@gmail.com>
2022-09-25 10:52:02 -0300

    Signer

---
+++ Signer.js
@@ -0,0 +1,20 @@
+class Signer {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  getApiKey() {
+    return this.apiKey;
+  }
+
+  async signMessage(msg) {
+    const signature = crypto
+      .createHmac("sha256", process.env.APP_SECRET)
+      .update(msg)
+      .digest("hex");
+
+    return signature;
+  }
+}
+
+module.exports.Signer = Signer;
